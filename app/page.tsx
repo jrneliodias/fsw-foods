c87727ac1209415cbf2504b35e4b17493e1f8b8c
Nélio Dias <jrneliodias@gmail.com>
2024-04-30 12:42:15 -0300

    feat: add Promo Banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import { Header } from "./_components/header";
 import Search from "./_components/search";
@@ -11,6 +12,17 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className=" pt-6">
+        <Image
+          src={"/promo_banner.svg"}
+          width={0}
+          height={0}
+          alt="Promoção de Pixxa"
+          sizes="100vw"
+          className="h-auto w-full"
+          quality={100}
+        />
       </div>
     </div>
   );
