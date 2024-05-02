d921dc4955b968a66cecdde3a0c71c9a1090d40b
Nélio Dias <jrneliodias@gmail.com>
2024-05-01 22:16:42 -0300

    style: add border radius to product page

--- app/products/[id]/_components/product-image.tsx
+++ app/products/[id]/_components/product-image.tsx
@@ -15,7 +15,7 @@ function ProductImage({ product }: ProductImageProps) {
   const handleBackClick = () => router.back();
 
   return (
-    <div className="relative h-[360px] w-full">
+    <div className="relative z-10 h-[360px] w-full">
       <Image
         src={product.imageUrl}
         alt={product.name}
