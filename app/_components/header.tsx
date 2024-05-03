d468e3f920094a7e4b16747f15162ffd810dc18c
Nélio Dias <jrneliodias@gmail.com>
2024-05-03 15:06:50 -0300

    feat: implement the home link in header logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src={"/fsw-foods.svg"} width={100} height={30} alt="FSW Foods" />
+      <Link href={"/"}>
+        <Image src={"/fsw-foods.svg"} width={100} height={30} alt="FSW Foods" />
+      </Link>
       <Button
         size={"icon"}
         variant={"outline"}
