64d52c611a80717ff5cde74df9d090bd5ba0b365
Nélio Dias <jrneliodias@gmail.com>
2024-05-03 16:07:04 -0300

    refactor: move link inside button to mentain the html semantic

--- app/page.tsx
+++ app/page.tsx
@@ -40,15 +40,16 @@ export default async function Home() {
       <div className=" space-y-3 pt-6">
         <div className="flex items-center justify-between px-5 ">
           <h2 className="font-semibold"> Pedidos Recomendados</h2>
-          <Link href={"/products/recommended"}>
-            <Button
-              variant={"ghost"}
-              className="text-primary hover:bg-transparent"
-            >
+          <Button
+            asChild
+            variant={"ghost"}
+            className="text-primary hover:bg-transparent"
+          >
+            <Link href={"/products/recommended"}>
               Ver todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
         <ProductList products={products} />
       </div>
@@ -58,15 +59,16 @@ export default async function Home() {
       <div className=" space-y-3 pt-6">
         <div className="flex items-center justify-between px-5 ">
           <h2 className="font-semibold"> Restaurantes Recomendados</h2>
-          <Link href="/restaurants/recommended">
-            <Button
-              variant={"ghost"}
-              className="text-primary hover:bg-transparent"
-            >
+          <Button
+            asChild
+            variant={"ghost"}
+            className="text-primary hover:bg-transparent"
+          >
+            <Link href="/restaurants/recommended">
               Ver todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
         <RestaurantList />
       </div>
