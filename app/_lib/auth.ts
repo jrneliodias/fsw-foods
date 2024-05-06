ef5139f8483bf4ab4d47ef53379c82148bc851e2
Nélio Dias <jrneliodias@gmail.com>
2024-05-06 17:40:59 -0300

    refactor: move auth options to new file and revalidate my-orders

---
+++ app/_lib/auth.ts
@@ -0,0 +1,21 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
+import { db } from "@/app/_lib/prisma";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = { ...session.user, id: user.id };
+      return session;
+    },
+  },
+};
